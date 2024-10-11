dc0123778f2bd7b10d6f24352ae4dbbfa90dbf25
yogeshwaran99 <itsyogeshwaran@gmail.com>
2024-10-11 07:53:08 +0530

    code profile

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -77,6 +77,27 @@ const About = () => {
       </svg>
     </li-icon>
   </a>
+  <a
+    className="code"
+    target="_blank"
+    href="https://codeforces.com/profile/yogeshwaran99"
+    rel="noopener noreferrer"
+  >
+    <strong>Codeforces profile</strong>
+    <li-icon aria-hidden="true" type="link-external" size="small">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        data-supported-dps="16x16"
+        fill="currentColor"
+        width="16"
+        height="16"
+        focusable="false"
+      >
+        <path d="M15 1v6h-2V4.41L7.41 10 6 8.59 11.59 3H9V1zm-4 10a1 1 0 01-1 1H5a1 1 0 01-1-1V6a1 1 0 011-1h2V3H5a3 3 0 00-3 3v5a3 3 0 003 3h5a3 3 0 003-3V9h-2z"></path>
+      </svg>
+    </li-icon>
+  </a>
 </div>
 
             <article className="about__card">
