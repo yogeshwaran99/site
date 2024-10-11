21c31b1429c1c08ad2d2d80d339ebbe9840fd1eb
yogeshwaran99 <itsyogeshwaran@gmail.com>
2024-10-11 07:20:22 +0530

    text updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
       <h2>About Me</h2>
       <div className="container about__container">        
         <div className="about__content">
-        <p>I am an aspiring Software Developer with a strong passion for contributing to innovative and dynamic teams. My enthusiasm for continuous learning drives me to stay updated with the latest industry trends and advancements. I thrive on tackling new challenges and am dedicated to honing my skills to deliver high quality software solutions. With a solid foundation in programming and a keen interest in emerging technologies, I am eager to bring my creativity and problem solving abilities to impactful projects.</p>        
+        <p>I am an aspiring Software Developer with a strong passion for contributing to innovative and dynamic teams. My enthusiasm for continuous learning drives me to stay updated with the latest industry trends and advancements. I am eager to bring my creativity and problem solving abilities to impactful projects.</p>        
         <div id="code-container">
   <h4>Coding Profiles:</h4>
   <a
