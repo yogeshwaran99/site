7082324c028c86c50058df1d1f0b74f255750daa
yogeshwaran99 <itsyogeshwaran@gmail.com>
2024-10-11 12:21:58 +0530

    coding profiles

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,113 +1,19 @@
 import React from 'react';
-import { FaGraduationCap } from 'react-icons/fa';
-
-
+import Code from './Code.jsx'
 import './about.css';
 
 const About = () => {
   return (
     <section id="about">
+      
       <h5>Get to know more</h5>
       <h2>About Me</h2>
       <div className="container about__container">        
         <div className="about__content">
         <p>I am an aspiring Software Developer with a strong passion for contributing to innovative and dynamic teams. My enthusiasm for continuous learning drives me to stay updated with the latest industry trends and advancements. I am eager to bring my creativity and problem solving abilities to impactful projects.</p>        
-        <div className="code-container">
-  <h4>Coding Profiles:</h4>
-  <a
-    className="code"
-    target="_blank"
-    href="https://hackerrank.com/profile/yogeshwaran99"
-    rel="noopener noreferrer"
-  >
-    <strong>Hackerrank profile</strong>
-    <li-icon aria-hidden="true" type="link-external" size="small">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        data-supported-dps="16x16"
-        fill="currentColor"
-        width="16"
-        height="16"
-        focusable="false"
-      >
-        <path d="M15 1v6h-2V4.41L7.41 10 6 8.59 11.59 3H9V1zm-4 10a1 1 0 01-1 1H5a1 1 0 01-1-1V6a1 1 0 011-1h2V3H5a3 3 0 00-3 3v5a3 3 0 003 3h5a3 3 0 003-3V9h-2z"></path>
-      </svg>
-    </li-icon>
-  </a>
-  <a
-    className="code"
-    target="_blank"
-    href="https://leetcode.com/u/yogeshwaran99"
-    rel="noopener noreferrer"
-  >
-    <strong>Leetcode profile</strong>
-    <li-icon aria-hidden="true" type="link-external" size="small">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        data-supported-dps="16x16"
-        fill="currentColor"
-        width="16"
-        height="16"
-        focusable="false"
-      >
-        <path d="M15 1v6h-2V4.41L7.41 10 6 8.59 11.59 3H9V1zm-4 10a1 1 0 01-1 1H5a1 1 0 01-1-1V6a1 1 0 011-1h2V3H5a3 3 0 00-3 3v5a3 3 0 003 3h5a3 3 0 003-3V9h-2z"></path>
-      </svg>
-    </li-icon>
-  </a>
-  <a
-    className="code"
-    target="_blank"
-    href="https://geeksforgeeks.org/user/yogeshwaran99/"
-    rel="noopener noreferrer"
-  >
-    <strong>Geeksforgeeks profile</strong>
-    <li-icon aria-hidden="true" type="link-external" size="small">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        data-supported-dps="16x16"
-        fill="currentColor"
-        width="16"
-        height="16"
-        focusable="false"
-      >
-        <path d="M15 1v6h-2V4.41L7.41 10 6 8.59 11.59 3H9V1zm-4 10a1 1 0 01-1 1H5a1 1 0 01-1-1V6a1 1 0 011-1h2V3H5a3 3 0 00-3 3v5a3 3 0 003 3h5a3 3 0 003-3V9h-2z"></path>
-      </svg>
-    </li-icon>
-  </a>
-  <a
-    className="code"
-    target="_blank"
-    href="https://codeforces.com/profile/yogeshwaran99"
-    rel="noopener noreferrer"
-  >
-    <strong>Codeforces profile</strong>
-    <li-icon aria-hidden="true" type="link-external" size="small">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        data-supported-dps="16x16"
-        fill="currentColor"
-        width="16"
-        height="16"
-        focusable="false"
-      >
-        <path d="M15 1v6h-2V4.41L7.41 10 6 8.59 11.59 3H9V1zm-4 10a1 1 0 01-1 1H5a1 1 0 01-1-1V6a1 1 0 011-1h2V3H5a3 3 0 00-3 3v5a3 3 0 003 3h5a3 3 0 003-3V9h-2z"></path>
-      </svg>
-    </li-icon>
-  </a>
-</div>
-
-            <article className="about__card">
-              <FaGraduationCap className="about__icon" />
-              <h5>Education</h5>
-              <small>Bachelor of Engineering</small><br></br>
-              <small> Electronics and Communication Engineering</small>
-            </article>            
-          </div>                    
-        </div>    
+        </div>
+      </div>  
+      <Code />
     </section>
   )
 }
---
+++ src/components/about/Code.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import  Hackerrank from '../../assets/hackerrank.png';
+import  Leetcode from '../../assets/leetcode.png';
+import  Geeksforgeeks from '../../assets/geeksforgeeks.png';
+import  Codeforces from '../../assets/codeforces.png';
+
+import './code.css';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const Code = () => {
+  const code = [
+    {
+      id: 1,
+      img: Hackerrank,      
+      platform: 'HackerRank',
+      problemssolved: '142',
+      link: 'https://www.hackerrank.com/profile/yogeshwaran99',      
+    },
+    {
+      id: 2,
+      img: Leetcode,      
+      platform: 'Leetcode',
+      problemssolved: '13',
+      link: 'https://leetcode.com/u/yogeshwaran99/',
+    },
+    {
+      id: 3,
+      img: Geeksforgeeks,      
+      platform: 'Geeksforgeeks',
+      problemssolved: '236',
+      link: 'https://www.geeksforgeeks.org/user/yogeshwaran99/',
+    },
+    {
+      id: 4,
+      img: Codeforces,      
+      platform: 'Codeforces',
+      problemssolved: '17',
+      link: 'https://codeforces.com/profile/yogeshwaran99',
+    },
+  ];
+
+  return (
+    <section id="code">
+      <h3>Coding Profiles</h3>      
+      <Swiper 
+        className="container code__container"
+        modules={[Pagination]}
+        spaceBetween={30} 
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+          600: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+          },
+        }}
+      >
+        {code.map((coding) => (
+          <SwiperSlide className="code" key={coding.id}>
+             <div className="code__img">
+                <img src={coding.img} alt={coding.platform} />
+              </div>
+            <h5 className='code__name'>{coding.platform}</h5>
+            <h4 className='code__name'>Problems Solved: {coding.problemssolved}</h4>
+            <a
+                  href={coding.link}
+                  target="_blank"
+                  className="btn"
+                  rel="noreferrer"
+                >
+                  visit
+                </a>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  )
+}
+
+export default Code;
