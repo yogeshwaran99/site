c4abc1849f9a7ebddc20d3c75c0cd82a8bda489e
yogeshwaran99 <itsyogeshwaran@gmail.com>
2024-10-15 00:48:51 +0530

    lc

--- src/components/about/Code.jsx
+++ src/components/about/Code.jsx
@@ -24,7 +24,7 @@ const Code = () => {
       id: 2,
       img: Leetcode,      
       platform: 'LeetCode',
-      problemssolved: '25',
+      problemssolved: '28',
       link: 'https://leetcode.com/u/yogeshwaran99/',
     },
     {
