31b4d4d813fe877cc2e7e1d94dba91fb40cf74b2
yogeshwaran99 <itsyogeshwaran@gmail.com>
2024-10-18 11:39:19 +0530

    resume btn

--- src/components/profile/CTA.jsx
+++ src/components/profile/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/Yogeshwaran.pdf';
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} target="_blank" rel="noopener noreferrer" className="btn">
+      <a href={CV} target="_blank" rel="noopener noreferrer" className="btn1">
         Resume
       </a>
     </div>
